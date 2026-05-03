09f9cd9b19926606210bf352cc677edd081a84ea
trustwells <157090522+trustwells@users.noreply.github.com>
2026-05-03 18:42:55 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        
+        vector<int> count(26, 0);
+        for (int i = 0; i < s.length(); ++i) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (auto& value : count) {
+            if (value != 0) return false;
+        }
+
+        return true;
+    }
+};
